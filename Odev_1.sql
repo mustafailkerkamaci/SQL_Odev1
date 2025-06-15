9ff2c72646c4afb1ff205ded87a8475063af7b79
Mustafa İlker Kamaci <mustafailkerkmc@gmail.com>
2025-06-15 03:22:08 +0300

    Add files via upload

---
+++ Odev_1.sql
@@ -0,0 +1,20 @@
+--SELECT title, description FROM film
+
+/*SELECT * FROM film
+WHERE length > 60 AND length <75;
+*/
+/*SELECT * FROM film
+WHERE rental_rate = 0.99 AND replacement_cost =12.99 OR replacement_cost = 28.99;
+*/
+
+/*SELECT * FROM customer
+WHERE first_name = 'Mary'
+*/
+
+SELECT * FROM film
+WHERE length <=50 AND rental_rate NOT IN (2.99, 4.99);
+
+
+
+
+
